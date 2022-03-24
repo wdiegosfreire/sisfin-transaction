060e24201d4fe48cf6ec7cc8b56a3e0732d6f131
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2022-03-24 14:58:17 -0300

    Added information in file "ImRunning.java"; created files "DateUtils.java" and "DatePatternEnum.java"; Path to "sisfin-maintenance" project on file "UserFeignClient.java" changed to be retrieved from enviroment variable.

--- src/main/java/br/com/dfdevforge/sisfintransaction/feignclients/UserFeignClient.java
+++ src/main/java/br/com/dfdevforge/sisfintransaction/feignclients/UserFeignClient.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.com.dfdevforge.common.entities.ResourceDataEntity;
 
 @Component
-//@FeignClient(name="sisfin-maintenance", url="http://localhost:8080", path="/userfeignserver")
-@FeignClient(name="sisfin-maintenance", url="https://sisfin-maintenance.herokuapp.com", path="/userfeignserver")
+@FeignClient(name="sisfin-maintenance", url="${SISFIN_URL_MAINTENANCE}", path="/userfeignserver")
 public interface UserFeignClient {
 	@GetMapping(value = "/{identity}")
 	public ResponseEntity<ResourceDataEntity> findByIdentity(@PathVariable long identity);
