720c6a7cfa6ec5c6c11ecf6ff3154c35a98667b6
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2022-04-06 18:36:36 -0300

    Created crud backend for module "PaymentMethod".

---
+++ src/main/java/br/com/dfdevforge/sisfintransaction/repositories/PaymentMethodRepository.java
@@ -0,0 +1,13 @@
+package br.com.dfdevforge.sisfintransaction.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import br.com.dfdevforge.sisfintransaction.entities.PaymentMethodEntity;
+
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethodEntity, Long>, JpaSpecificationExecutor<PaymentMethodEntity> {
+	public PaymentMethodEntity findByIdentity(Long identity);
+	public List<PaymentMethodEntity> findByUserIdentity(Long userIdentity);
+}
