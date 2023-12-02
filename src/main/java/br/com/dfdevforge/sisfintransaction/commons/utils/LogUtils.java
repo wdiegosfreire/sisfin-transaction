b7a501d3905a0715becc7a177f579137175d0c85
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2023-12-02 19:14:54 -0300

    Corrigindo issues do Sonar.

--- src/main/java/br/com/dfdevforge/sisfintransaction/commons/utils/LogUtils.java
+++ src/main/java/br/com/dfdevforge/sisfintransaction/commons/utils/LogUtils.java
@@ -7,8 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import br.com.dfdevforge.sisfintransaction.commons.entities.UserEntity;
-
 public class LogUtils {
 	private static final String LOGGER_PATTERN = "Sisfin Transaction Error: {}";
 	
@@ -28,26 +26,21 @@ public class LogUtils {
 		LOGGER.warn(LOGGER_PATTERN, message);
 	}
 
-	public void stackTrace(Exception exception, UserEntity user) {
+	public void stackTrace(Exception exception) {
 		final String TAB = "\t";
 		final String BREAK = "\n";
 		Date now = new Date();
 		StringBuilder stackTrace = new StringBuilder();
 
-		String message = "User not found";
-		if (user != null && user.getName() != null)
-			message = user.getName();
-
 		stackTrace.append(TAB + "Exception: " + exception + BREAK);
-		stackTrace.append(TAB + "User: " + message + BREAK + BREAK);
-		
+
 		for (StackTraceElement stackTraceElement : exception.getStackTrace()) {
 			if (StringUtils.contains(stackTraceElement.getClassName(), "dfdevforge")) {
 				stackTrace.append(TAB + stackTraceElement + BREAK);
 			}
 		}
 
-		error("Inicio log " + now.getTime() + BREAK + BREAK + stackTrace.toString());
-		error("Fim log " + now.getTime() + BREAK);
+		this.error("Inicio log " + now.getTime() + BREAK + BREAK + stackTrace.toString());
+		this.error("Fim log " + now.getTime() + BREAK);
 	}
 }
