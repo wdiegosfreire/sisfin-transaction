68aed82b1064ecb5ab2ec6c7cc07b6027e21d4c1
wdiegosfreire <wdiegosfreire@yahoo.com.br>
2025-06-02 16:59:55 -0300

    Created filter by source value range and location on Objective module.

--- src/main/java/br/com/dfdevforge/sisfintransaction/transaction/model/objectivemovement/repositories/ObjectiveMovementRepositorySelectByPeriodAndDynamicFilters.java
+++ src/main/java/br/com/dfdevforge/sisfintransaction/transaction/model/objectivemovement/repositories/ObjectiveMovementRepositorySelectByPeriodAndDynamicFilters.java
@@ -1,5 +1,6 @@
 package br.com.dfdevforge.sisfintransaction.transaction.model.objectivemovement.repositories;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
@@ -15,7 +16,10 @@ import br.com.dfdevforge.sisfintransaction.transaction.model.objectivemovement.e
 
 @Repository
 public class ObjectiveMovementRepositorySelectByPeriodAndDynamicFilters {
+	private static final String LOCATION_IDENTITY = "locationIdentity";
 	private static final String ACCOUNT_SOURCE_IDENTITY = "accountSourceIdentity";
+	private static final String VALUE_START = "valueStart";
+	private static final String VALUE_END = "valueEnd";
 
 	private final EntityManager entityManager;
 
@@ -36,8 +40,10 @@ public class ObjectiveMovementRepositorySelectByPeriodAndDynamicFilters {
 		whereClause.append("   (obm.paymentDate is null and obm.dueDate between :startDate and :endDate) ");
 		whereClause.append(" ) ");
 
-		if (Utils.value.exists(filterMap, ACCOUNT_SOURCE_IDENTITY))
-			whereClause.append(" and obm.accountSource.identity = :accountSourceIdentity ");
+		whereClause.append(" and (obm.accountSource.identity = :accountSourceIdentity or :accountSourceIdentity is null) ");
+		whereClause.append(" and (obm.value >= :valueStart or :valueStart is null) ");
+		whereClause.append(" and (obm.value <= :valueEnd or :valueEnd is null) ");
+		whereClause.append(" and (obm.objective.location.identity = :locationIdentity or :locationIdentity is null) ");
 
 		StringBuilder jpql = new StringBuilder();
 
@@ -48,9 +54,10 @@ public class ObjectiveMovementRepositorySelectByPeriodAndDynamicFilters {
 		query.setParameter("userIdentity", userIdentity);
 		query.setParameter("startDate", this.findStartDate(periodDate));
 		query.setParameter("endDate", this.findEndDate(periodDate));
-
-		if (Utils.value.exists(filterMap, ACCOUNT_SOURCE_IDENTITY))
-			query.setParameter(ACCOUNT_SOURCE_IDENTITY, Long.parseLong(filterMap.get(ACCOUNT_SOURCE_IDENTITY)));
+		query.setParameter(ACCOUNT_SOURCE_IDENTITY, Utils.value.exists(filterMap, ACCOUNT_SOURCE_IDENTITY) ? Long.parseLong(filterMap.get(ACCOUNT_SOURCE_IDENTITY)) : null);
+		query.setParameter(VALUE_START, Utils.value.exists(filterMap, VALUE_START) ? new BigDecimal(filterMap.get(VALUE_START)) : null);
+		query.setParameter(VALUE_END, Utils.value.exists(filterMap, VALUE_END) ? new BigDecimal(filterMap.get(VALUE_END)) : null);
+		query.setParameter(LOCATION_IDENTITY, Utils.value.exists(filterMap, LOCATION_IDENTITY) ? Long.parseLong(filterMap.get(LOCATION_IDENTITY)) : null);
 
 		return query.getResultList();
 	}
